0bce1311bfe7a390e4e9f429d42a4f46934b1373
brosdee21 <nwaokolodennis90@gmail.com>
2025-07-25 21:40:16 +0100

    -update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,7 +9,7 @@ import TrackVisibility from 'react-on-screen';
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+    const toRotate = ["MERN Stack Developer", "DevOps Engineer", "Software Engineer"];
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100);
     const period = 2000; 
