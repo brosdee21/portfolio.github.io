00a9e68e2a94a53d824b652af857af404f5992a9
lentogroup <etcojemeni@gmail.com>
2026-03-01 21:59:41 +0100

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -96,9 +96,7 @@
 import { useEffect, useState } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/img/logo.svg";
-import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import { FaGithub, FaLinkedin, FaInstagram} from 'react-icons/fa'
 
  export const NavBar = () => {
     const [activeLink, SetActiveLink] = useState('home');
@@ -143,9 +141,9 @@ import navIcon3 from "../assets/img/nav-icon3.svg";
 
                     <span className="navbar-text">
                 <div className="social-icon">
-                    <a href="#a"><img src={navIcon1} alt="" /></a>
-                    <a href="#a"><img src={navIcon2} alt="" /></a>
-                    <a href="#a"><img src={navIcon3} alt="" /></a>
+                    <a href="https://www.instagram.com/bros_dee3/"><FaInstagram /></a>
+                    <a href="https://www.linkedin.com/in/dennis-nwaokolo/"><FaLinkedin /></a>
+                    <a href="https://github.com/brosdee21"><FaGithub /></a>
                 </div>
                 <div style={{ display: 'flex', gap: '10px' }}>
                     <button className="vvd" onClick={() => console.log('connect')}>
